4b0c10e3728527d92fd640014ab32d0514b8f2f0
Wizard77-exe <jhayedc0@gmail.com>
2026-02-12 02:05:05 +0800

    updating source code

--- BasicProjects/attendance_monitoring/attendance_monitoring.c
+++ BasicProjects/attendance_monitoring/attendance_monitoring.c
@@ -7,19 +7,22 @@
 #define MAX_NAME_LENGTH 50
 
 // defining our Record struct
-typedef struct {
+typedef struct
+{
     int id;
     char name[MAX_NAME_LENGTH];
-    int isPresent; 
+    int isPresent;
 } Record;
 
 // Initializing an array of struct we defined above
 Record attendance[MAX_RECORD_LENGTH];
-int recordCount = 0;    // number of the record present
+int recordCount = 0; // number of the record present
 
-//function to add a record
-void addRecord() {
-    if (recordCount >= MAX_RECORD_LENGTH) {
+// function to add a record
+void addRecord()
+{
+    if (recordCount >= MAX_RECORD_LENGTH)
+    {
         printf("Cannot add record, List is full!\n");
         return;
     }
@@ -27,16 +30,27 @@ void addRecord() {
     printf("Enter Student ID: ");
     scanf("%d", &newRecord.id);
     // Checks if a student with the same id already occurs
-    for (int i = 0; i < recordCount; i++) {
-        if (attendance[i].id == newRecord.id) {
+    for (int i = 0; i < recordCount; i++)
+    {
+        if (attendance[i].id == newRecord.id)
+        {
             printf("A Student with the same ID already exists!\n\n");
             return;
         }
     }
+    // flush leftover newline
+    int ch;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+        ;
     printf("Enter Student Name: ");
-    scanf("%s", newRecord.name);
-    printf("Enter 1 if present and 0 if absent: ");
-    scanf("%d", &newRecord.isPresent);
+    fgets(newRecord.name, MAX_NAME_LENGTH, stdin);
+    newRecord.name[strcspn(newRecord.name, "\n")] = '\0';
+    // checking if the user input 0 or 1 only.
+    do
+    {
+        printf("Enter 1 if present and 0 if absent: ");
+        scanf("%d", &newRecord.isPresent);
+    } while (newRecord.isPresent != 0 && newRecord.isPresent != 1);
 
     printf("\n\n Record Added Successfully!\n\n");
     attendance[recordCount] = newRecord;
@@ -44,43 +58,56 @@ void addRecord() {
 }
 
 // function to display all the records
-void displayRecord() {
-    if (recordCount <= 0) {
+void displayRecord()
+{
+    if (recordCount <= 0)
+    {
         printf("Record is currently Empty!\n");
         return;
     }
-    printf("ID\tName\t\tRemark\n");
-    printf("------------------------------\n");
-    for (int i = 0; i < recordCount; i++) {
-        printf("%d\t%s\t\t%s\n", attendance[i].id, attendance[i].name, attendance[i].isPresent?"Present":"Absent");
+    printf("%-5s %-20s %-7s\n", "ID", "Name", "Remark");
+    printf("----------------------------------------\n");
+    for (int i = 0; i < recordCount; i++)
+    {
+        printf("%-5d %-20s %-7s\n", attendance[i].id, attendance[i].name, attendance[i].isPresent ? "Present" : "Absent");
+        ;
     }
     printf("\n");
 }
 
 // function to search for a specific record using an ID
-void searchRecord() {
-    int s_id; 
+void searchRecord()
+{
+    int s_id;
     printf("Enter Record ID: ");
     scanf("%d", &s_id);
-    for (int i = 0; i < recordCount; i++) {
-        if (attendance[i].id == s_id) {
+    for (int i = 0; i < recordCount; i++)
+    {
+        if (attendance[i].id == s_id)
+        {
             printf("Record Found!\n");
             printf("Student ID: %d\n", attendance[i].id);
             printf("Student Name: %s\n", attendance[i].name);
-            printf("Remark: %s\n\n", attendance[i].isPresent?"Present":"Absent");
+            printf("Remark: %s\n\n", attendance[i].isPresent ? "Present" : "Absent");
+            return;
         }
     }
+    printf("Record Not Found\n\n");
 }
 
 // delete a record with an ID
-void deleteRecord() {
+void deleteRecord()
+{
     int s_id;
     printf("Enter the ID you wanna delete: ");
     scanf("%d", &s_id);
-    for (int i = 0; i < recordCount; i++) {
-        if (attendance[i].id == s_id) {
-            for (int j = i; j < recordCount - 1; j++) {
-                attendance[j] = attendance[j+1];
+    for (int i = 0; i < recordCount; i++)
+    {
+        if (attendance[i].id == s_id)
+        {
+            for (int j = i; j < recordCount - 1; j++)
+            {
+                attendance[j] = attendance[j + 1];
             }
             recordCount--;
             printf("Record Deleted!\n\n");
@@ -89,12 +116,14 @@ void deleteRecord() {
     }
 }
 
-int main() {
+int main()
+{
 
     int choice;
 
     // App loop
-    while(1) {
+    while (1)
+    {
         printf("Attendance Monitoring\n");
         printf("=====================\n");
         printf("1. Add Record\n");
@@ -105,15 +134,27 @@ int main() {
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch(choice) {
-            case 1: addRecord(); break;
-            case 2: displayRecord(); break;
-            case 3: searchRecord(); break;
-            case 4: deleteRecord(); break;
-            case 5: exit(0); break;
-            default: printf("Invalid Choice Motherfucker!\n");
+        switch (choice)
+        {
+        case 1:
+            addRecord();
+            break;
+        case 2:
+            displayRecord();
+            break;
+        case 3:
+            searchRecord();
+            break;
+        case 4:
+            deleteRecord();
+            break;
+        case 5:
+            exit(0);
+            break;
+        default:
+            printf("Invalid Choice Motherfucker!\n");
         }
     }
-    
+
     return 0;
 }
