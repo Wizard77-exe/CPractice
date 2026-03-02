d75ab77cb47d17774c7ace099ffb7a71382f696c
Wizard77-exe <jhayedc0@gmail.com>
2026-03-02 20:48:04 +0800

    displayItems()

--- BasicProjects/inventory-system/inventory-system.c
+++ BasicProjects/inventory-system/inventory-system.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #define MAX_LEN 30
+#define BUFFER_SIZE 256
 #define FILENAME "inventory.txt"
 
 void flush_input()
@@ -46,12 +47,51 @@ void addItem()
         perror("Fclose");
         return;
     }
-    printf("\x1b[33mSuccessfully Added an Item\n\x1b[0m");
+    printf("\x1b[34mSuccessfully Added an Item\x1b[0m\n\n");
+    return;
+}
+
+void displayItems() {
+    FILE *file = fopen(FILENAME, "r");
+    if (!file) {
+        perror("fopen");
+        return;
+    }
+    
+    char buffer[BUFFER_SIZE];
+    printf("\n");
+    while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
+        printf("%s", buffer);
+    }
+
+    if (fclose(file) != 0) {
+        perror("fclose");
+        return;
+    }
+    printf("\n\x1b[34mSuccessfully Displayed Items\x1b[0m\n\n");
     return;
 }
 
 int main()
 {
-    addItem();
+    int choice;
+    while(1) {
+        printf("Inventory System\n");
+        printf("================\n");
+        printf("1. Add Item\n");
+        printf("2. Display Items\n");
+        printf("0. Exit\n");
+
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+        flush_input();
+
+        switch(choice) {
+            case 1: addItem(); break;
+            case 2: displayItems(); break;
+            case 0: exit(0); break;
+            default: printf("Invalid Choice\n");
+        }
+    }
     return 0;
 }
