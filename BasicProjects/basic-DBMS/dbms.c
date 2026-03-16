9319fe5d9ae4649565ac0e34e0be4640db95aab6
Wizard77-exe <jhayedc0@gmail.com>
2026-03-16 23:55:12 +0800

    update: main README.md

---
+++ BasicProjects/basic-DBMS/dbms.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+// Singly Linked List Structure
+typedef struct {
+    int data;
+    SNode *next;
+} SNode;
+
+// doubly Linked List Structure
+typedef struct {
+    int data; 
+    DNode *next;
+    DNode *prev;
+} DNode;
+
+typedef struct {
+    
+} Vector;
+
+int main() {
+
+    return 0;
+}
